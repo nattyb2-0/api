2e98cb1ef22cf0382bf93c9378fb0a44daf25377
Natty Davis <natty.davis79@gmail.com>
2017-05-09 18:19:37 -0400

    leggo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const path = require('path');
+const logger = require('morgan');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -6,12 +8,14 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
