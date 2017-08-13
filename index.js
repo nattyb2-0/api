18bebb03c96413a476c94988092aa7226983ea0c
Natty Davis <natty.davis79@gmail.com>
2017-08-13 14:58:23 -0400

    new

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 const path = require('path');
 const logger = require('morgan');
-var app = express();
+const psql = require('./db/db.js');
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
 
+var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -26,6 +29,17 @@ app.get('/api',(req,res)=>{
 // app.get('/', function(request, response) {
 //   response.render('pages/index');
 // });
+app.post('/signup',(req, res, next)=> {
+    const SALTROUNDS = 10;
+    console.log(req.body);
+    const userObject = {
+      username: req.body.username,
+      password: bcrypt.hashSync(req.body.password, SALTROUNDS)
+    };
+    psql.one(`INSERT INTO users ( username, password )
+    VALUES ('${userObject.username}', '${userObject.password}') returning *;`)
+})
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
